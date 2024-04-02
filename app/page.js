74994844b38db4d2d00c2da0c431387238d2534d
Zhang Zeyu <zhangzeyu2001@gmail.com>
2024-04-02 17:14:39 +0100

    fix: styles when in light mode

--- app/page.js
+++ app/page.js
@@ -102,7 +102,7 @@ const App = () => {
         {loggedIn ? <Sidebar notes={notes} onItemSelected={handleItemSelected} selectedItem={selectedItem} /> : <UnauthSidebar />}
         <div className="w-full flex flex-wrap items-center justify-between mx-auto">
           <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 bg-gray-50 md:flex-row rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 bg-gray-50 md:flex-row rtl:space-x-reverse md:mt-0 md:border-0 bg-gray-800 md:bg-gray-900 border-gray-700">
               <li>
                 <button className={`block py-2 px-3 ${selection === 'Transcript' ? 'bg-gray-100 text-blue-700' : 'hover:bg-gray-100 hover:text-blue-700 text-blue-400'} hover:bg-transparent border-0 p-0`}
                   onClick={() => setSelection('Transcript')}>Transcript</button>
